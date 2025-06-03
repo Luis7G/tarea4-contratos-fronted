d5ad8a1cc7d4ca18b665ea8d56461fcf6441b158
Joel Guingla <joel.guingla7@gmail.com>
2025-06-02 21:06:08 -0500

    feat: update project configuration and dependencies, enhance routing, and add polyfills

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { PlantillaWordComponent } from './plantilla-word/contrato-adquisicion-bienes.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'contrato-adquisicion-bienes', pathMatch: 'full' }, // Ruta por defecto
+  { path: '', redirectTo: '/contrato-adquisicion-bienes', pathMatch: 'full' },
   { path: 'contrato-adquisicion-bienes', component: PlantillaWordComponent },
+  { path: '**', redirectTo: '/contrato-adquisicion-bienes' }, // Wildcard route
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false, // Solo para debug
+      useHash: false,
+    }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
